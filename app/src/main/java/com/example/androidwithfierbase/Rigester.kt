241d9797dc3b64e9c82356e934b0ffd9c3081072
Nada Abo Mostfa <N.abom@college.labs>
2024-07-08 20:23:10 +0300

    first commit

---
+++ app/src/main/java/com/example/androidwithfierbase/Rigester.kt
@@ -0,0 +1,11 @@
+package com.example.androidwithfierbase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Rigester : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rigester)
+    }
+}
